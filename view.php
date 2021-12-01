7086e9d8a5bc00d32121b14a19d2f2bd9fa7ef07
Yusree <fireace.yk@gmail.com>
2021-12-01 19:59:56 +0200

    Add files via upload

---
+++ view.php
@@ -0,0 +1,46 @@
+<?php
+require('db.php');
+include("auth.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>View Records</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div class="form">
+<p><a href="index.php">Home</a> 
+| <a href="insert.php">Insert New Record</a> 
+| <a href="logout.php">Logout</a></p>
+<h2>View Records</h2>
+<table width="50%" border="1" style="border-collapse:collapse;">
+<thead>
+<tr>
+<th><strong>S.No</strong></th>
+<th><strong>Name</strong></th>
+<th><strong>Age</strong></th>
+<th><strong>Actions</strong></th>
+
+</tr>
+</thead>
+<tbody>
+<?php
+$count=1;
+$sel_query="Select * from new_record ORDER BY id desc;";
+$result = mysqli_query($con,$sel_query);
+while($row = mysqli_fetch_assoc($result)) { ?>
+<tr><td align="center"><?php echo $count; ?></td>
+<td align="center"><?php echo $row["name"]; ?></td>
+<td align="center"><?php echo $row["age"]; ?></td>
+<td align="center">
+<a href="edit.php?id=<?php echo $row["id"]; ?>">Edit</a> <a href="delete.php?id=<?php echo $row["id"]; ?>">Delete</a>
+</td>
+</tr>
+<?php $count++; } ?>
+</tbody>
+</table>
+</div>
+</body>
+</html>
